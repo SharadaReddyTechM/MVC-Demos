ca2ff22c21e77224ae857a74c36b8b92e1dbf9a6
admin2112 <admin2112@admin2112>
2022-01-20 20:50:29 +0530

    Add project files.

---
+++ DemoLINQ_Usage_CRUD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoLINQ_Usage_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
